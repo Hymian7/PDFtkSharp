a8c04b4e81f58e356a54fc82af4ccfa7a4b706f6
Lukas Möller <lukasmoeller1998@googlemail.com>
2022-01-11 15:34:59 +0100

    Add Linux support for executable discovery and paths

--- PDFtkSharp/PDFManipulator.cs
+++ PDFtkSharp/PDFManipulator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace PDFtkSharp
 {
@@ -25,9 +26,45 @@ namespace PDFtkSharp
         /// <summary>
         /// Use this constructor to use the shipped PDFtk EXE file
         /// </summary>
-        public PDFManipulator()
+        public PDFManipulator() 
         {
-            ExecutablePath = new FileInfo(".\\assets\\pdftk.exe");
+            // Set executable path based on OS
+
+            // Windows: Check subdirectory in case the binaries are provided
+            // Also check Default pdftk installation directory
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (System.IO.File.Exists(".\\assets\\pdftk.exe"))
+                {
+                    ExecutablePath = new FileInfo(".\\assets\\pdftk.exe");
+                }
+                else if (System.IO.File.Exists(Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\PDFtk\bin\pdftk.exe")))
+                {
+                    ExecutablePath = new FileInfo(Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\PDFtk\bin\pdftk.exe"));
+                }
+                else
+                {
+                    throw new FileNotFoundException("pdftk executable could not be located");
+                }
+            }
+            // Linux: pdftk is normally installed to /bin/pdftk
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                if(File.Exists("/bin/pdftk"))
+                {
+                     ExecutablePath = new FileInfo("/bin/pdftk");
+                }
+                else
+                {
+                    throw new FileNotFoundException("pdftk executable could not be located");
+                }
+            }
+            else
+            {
+                throw new Exception("OS not supported");
+            }
+            System.Diagnostics.Debug.WriteLine("PDF Manipulator Executable Path set to: " + ExecutablePath.FullName);
+
         }
 
         /// <summary>
@@ -36,7 +73,15 @@ namespace PDFtkSharp
         /// <param name="_exePath">Full Path to PDFtk Server EXE</param>
         public PDFManipulator(FileInfo _exePath)
         {
+            //Check if executable path exists. Otherwise throw exception
+            if(!_exePath.Exists)
+            {
+                throw new FileNotFoundException($"The Pdftk executable could not be found under {_exePath}");
+            }
+
             ExecutablePath = _exePath;
+
+            System.Diagnostics.Debug.WriteLine("PDF Manipulator Executable Path set to: " + ExecutablePath.FullName);
         }
 
     }
